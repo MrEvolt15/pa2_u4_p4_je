7ddfa19625e1ed648fab13fb92d60c1ae932160b
MrEvolt15 <joelespinosa1a@hotmail.com>
2023-08-21 20:15:41 -0500

    Taller41

---
+++ src/main/java/com/example/demo/service/to/TransferenciaServiceTO.java
@@ -0,0 +1,12 @@
+package com.example.demo.service.to;
+
+import java.math.BigDecimal;
+
+public class TransferenciaServiceTO {
+    private String numeroCtaOrigen;
+    private String numeroCtaDestino;
+    private BigDecimal monto;
+
+    //SET Y GET
+    
+}
